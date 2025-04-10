05f3874122230cef3c9e0009bef2536475136f36
tewarishash12 <tewarishashwat2002@gmail.com>
2025-04-10 18:58:30 +0530

    updated schema based on the requirement

--- backend/auth_server/models/User.js
+++ backend/auth_server/models/User.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name:{type:String, required:true},
-    email: { type: String, required: true, unique: true },
+    name:{type:String, required:true, trim:true},
+    email: { type: String, required: true, unique: true, lowercase:true, trim:true },
     password: { type: String, required: true },
 });
 
