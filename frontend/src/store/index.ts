cd28987a9b9e900b05fd496ecebfc536aa06e491
tewarishash12 <tewarishashwat2002@gmail.com>
2025-07-03 21:53:59 +0530

    setting up redux store for the flow of information

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from '@/store/store';
+import { TypedUseSelectorHook,useDispatch,useSelector } from 'react-redux'; 
 
 const store = configureStore({
     reducer: rootReducer,
@@ -10,7 +11,10 @@ const store = configureStore({
     devTools: process.env.NODE_ENV !== 'production',
 });
 
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
+type AppDispatch = typeof store.dispatch;
+type RootState = ReturnType<typeof store.getState>;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export default store;
--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,13 +1,16 @@
 import { combineReducers } from '@reduxjs/toolkit';
-import userReducer from '@/features/user/userSlice';
-import interviewReducer from '@/features/interview/interviewSlice';
+import authReducer from '@/features/auth/authSlice';
+import resumeReducer from "@/features/resume/resumeSlice"
+import questionReducer from "@/features/questions/questionSlice"
 import audioReducer from '@/features/audio/audioSlice';
-import feedbackReducer from '@/features/feedback/feedbackSlice';
+import interviewReducer from '@/features/interview/interviewSlice';
 
 const rootReducer = combineReducers({
-    user: userReducer,
-    interview: interviewReducer,
+    auth: authReducer,
+    resume: resumeReducer,
+    question:questionReducer,
     audio: audioReducer,
-    feedback: feedbackReducer,});
+    interview: interviewReducer,
+});
 
 export default rootReducer;
