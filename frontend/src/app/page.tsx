55e9267b5217b9fa555613623cc315db94b7166d
tewarishash12 <tewarishashwat2002@gmail.com>
2025-07-03 21:09:06 +0530

    updated the code and remove unwanted code

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,11 @@
+import HeroSection from "@/app/components/HeroSection";
+import FeatureHighlights from "@/app/components/FeaturedHighlights";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <></>
+    <div>
+      <HeroSection />
+      <FeatureHighlights />
+    </div>
   );
 }
