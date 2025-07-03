c927f6607f980840d92d8f90b5b350aa0225402f
tewarishash12 <tewarishashwat2002@gmail.com>
2025-07-03 21:02:32 +0530

    updated path of saving of code

--- backend/main_server/controllers/resumeController.js
+++ backend/main_server/controllers/resumeController.js
@@ -1,12 +1,7 @@
 const Resume = require('../models/Resume');
 const resumeParser = require('../services/resumeParser');
-const path = require('path');
-const fs = require('fs');
 
-// @desc    Upload and parse a resume
-// @route   POST /api/resumes/upload
-// @access  Private
-exports.uploadResume = async (req, res, next) => {
+exports.uploadResume = async (req, res) => {
     try {
         if (!req.file) {
             return res.status(400).json({ message: 'No file uploaded' });
@@ -14,25 +9,25 @@ exports.uploadResume = async (req, res, next) => {
 
         const extractedText = await resumeParser(req.file.path);
 
+        const relativePath = req.file.path.replace(/\\/g, '/').split('uploads')[1];
+        const publicPath = `/uploads${relativePath}`;
+
         const resume = new Resume({
             user: req.user._id,
             fileName: req.file.originalname,
-            filePath: req.file.path,
+            filePath: publicPath,
             extractedText,
         });
 
         await resume.save();
 
-        res.status(201).json({ message: 'Resume uploaded and parsed', resumeId: resume._id, extractedText });
+        res.status(201).json({ message: 'Resume uploaded and parsed', resumeId:resume._id, resume:resume, extractedText:extractedText});
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
 };
 
-// @desc    Get all resumes uploaded by the logged-in user
-// @route   GET /api/resumes/user
-// @access  Private
-exports.getUserResumes = async (req, res, next) => {
+exports.getUserResumes = async (req, res) => {
     try {
         const resumes = await Resume.find({ user: req.user._id }).sort({ uploadedAt: -1 });
         res.json(resumes);
