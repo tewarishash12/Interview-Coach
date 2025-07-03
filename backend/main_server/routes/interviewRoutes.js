b2d121002a7e5b89a102a4beff2826a932560413
tewarishash12 <tewarishashwat2002@gmail.com>
2025-07-03 21:03:39 +0530

    updated route to better suit it's purpose

--- backend/main_server/routes/interviewRoutes.js
+++ backend/main_server/routes/interviewRoutes.js
@@ -1,11 +1,11 @@
 // routes/interviewRoutes.js
 const express = require('express');
 const router = express.Router();
-const { createInterview, getUserInterviews, getInterviewById } = require('../controllers/interviewController');
+const { saveInterview, getUserInterviews, getInterviewById } = require('../controllers/interviewController');
 const { authMiddleware } = require('../middleware/authMiddleware');
 
 // All routes protected via JWT auth
-router.post('/', authMiddleware, createInterview);
+router.post('/save', authMiddleware, saveInterview);
 router.get('/', authMiddleware, getUserInterviews);
 router.get('/:id', authMiddleware, getInterviewById);
 
