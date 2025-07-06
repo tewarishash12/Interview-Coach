4e61b328e584760e7ee7c404dc49405084f601b0
tewarishash12 <tewarishashwat2002@gmail.com>
2025-07-06 15:10:12 +0530

    added controller from verify-email

--- backend/auth_server/routes/authRoutes.js
+++ backend/auth_server/routes/authRoutes.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
 
-const { register, login, refreshToken, logout } = require('../controllers/authControllers.js');
+const { register, login, refreshToken, logout , verifyEmail} = require('../controllers/authControllers.js');
 
 router.post('/register', register);
 router.post('/login', login);
 router.post('/token', refreshToken);
 router.post('/logout', logout);
+router.get('/verify-email', verifyEmail);
 
 module.exports = router
