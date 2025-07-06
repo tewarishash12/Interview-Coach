37b1e7c84d3b3694f7ac0cf31c45a35be102f31d
tewarishash12 <tewarishashwat2002@gmail.com>
2025-07-06 15:09:54 +0530

    added verified field to make sure authenticated user logs into website

--- backend/auth_server/models/User.js
+++ backend/auth_server/models/User.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name:{type:String, required:true, trim:true},
-    email: { type: String, required: true, unique: true, lowercase:true, trim:true },
+    name: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true, lowercase: true, trim: true },
     password: { type: String, required: true },
+    isVerified: { type: Boolean, default: false },
+    verificationToken: { type: String }
 });
 
 module.exports = mongoose.model("User", userSchema)
