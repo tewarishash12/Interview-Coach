c7e60c6334996a9bca694ca8edd283f72efb25db
tewarishash12 <tewarishashwat2002@gmail.com>
2025-08-11 22:39:35 +0530

    getInterviewById route was not connected

--- backend/main_server/routes/interviewRoutes.js
+++ backend/main_server/routes/interviewRoutes.js
@@ -7,7 +7,7 @@ const { authMiddleware } = require('../middleware/authMiddleware');
 // All routes protected via JWT auth
 router.post('/save', authMiddleware, saveInterview);
 router.get('/', authMiddleware, getUserInterviews);
-router.get('/', authMiddleware, getUserInterviews);
+router.get('/:id', authMiddleware, getInterviewById);
 router.delete('/delete/:id', authMiddleware, deleteInterview);
 
 module.exports = router;
